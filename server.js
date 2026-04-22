79d8d5071fb1f09b6e6a1cbae1625027700c39ac
Gioele <gioybonny20@gmial.com>
2026-04-22 13:15:12 +0100

    Image upload functionality added

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 'use strict';
 
 import express from 'express';
+import routes from "./routes.js";
 import logger from "./utils/logger.js";
-import routes from './routes.js';
 import { create } from 'express-handlebars';
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(cookieParser());
+app.use(fileUpload({useTempFiles: true}));
 
 const handlebars = create({
     extname: '.hbs',
     helpers: {
+
         uppercase: (inputString) => {
             return inputString.toUpperCase();
         },
@@ -36,11 +40,15 @@ const handlebars = create({
             let message = rating >= 4 ? "Popular with listeners!" : "";
             return message;
         },
-    },
+
+    }
 });
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 
+
 app.use("/", routes);
 
 app.listen(port, () => logger.info(`Your app is listening on port ${port}`));
+
+    
